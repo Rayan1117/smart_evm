ff9caa7868462f8ac156b5e6532f3e78e1afd500
rayan <rayanraja7279@gmail.com>
2024-10-03 18:13:26 +0530

    initial-r

--- lib/hive_setup.dart
+++ lib/hive_setup.dart
@@ -1,9 +1,8 @@
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveSetup {
   static Future<void> initHive() async {
     await Hive.initFlutter();
-    await Hive.openBox('myBox'); // You can name your box anything you like
+    await Hive.openBox('curr-state');
   }
 }
