1d3bc4d51bfdf1d00107d6aeac2bc3a6fb689bf5
rayan <rayanraja7279@gmail.com>
2024-10-03 19:41:14 +0530

    analytics-

--- lib/hive_setup.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-class HiveSetup {
-  static Future<void> initHive() async {
-    await Hive.initFlutter();
-    await Hive.openBox('curr-state');
-  }
-}
