ed1e34d5e576f73249b29bbfd64264593b481de2
rayan <rayanraja7279@gmail.com>
2024-10-03 19:14:21 +0530

    finished fixed page state

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_evm/hive_setup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_evm/firebase_options.dart';
+import 'package:smart_evm/home_page/home_page.dart';
 import 'package:smart_evm/login_page/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await HiveSetup.initHive(); // Initialize Hive
-  runApp(const MyApp());
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+  String? espId = prefs.getString('espId');
+
+  runApp(MyApp(isLoggedIn: isLoggedIn, espId: espId));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  final String? espId;
+
+  const MyApp({Key? key, required this.isLoggedIn, this.espId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page',
+      title: 'Smart EVM',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: isLoggedIn
+          ? CandidateListPage(espId: espId) // If logged in, navigate to CandidateListPage
+          : const LoginPage(), // If not logged in, navigate to LoginPage
       debugShowCheckedModeBanner: false,
     );
   }
